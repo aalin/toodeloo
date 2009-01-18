93bf7eb6e5ccfe5506873ea7c1498b5bc66757b7
Andreas Alin <andreas.alin@gmail.com>
2009-01-18 21:18:02 +0100

    ...

--- src/player.cpp
+++ src/player.cpp
@@ -12,7 +12,7 @@ Player::Player(PlayState& state)
 	body->addShape(_bottom);
 
 	_bottom = new Chipmunk::Shapes::Circle(*body, 8.0, cpv(0.0, 0.0));
-	_bottom->elasticity(0.1);
+	_bottom->elasticity(0.0);
 	_bottom->friction(1.0);
 	body->addShape(_bottom);
 
@@ -27,8 +27,6 @@ Player::Player(PlayState& state)
 	_bodies["left_hand"] = new Chipmunk::Body(state.space(), 10.0, 50.0);
 	body = _bodies["left_hand"];
 	shape = new Chipmunk::Shapes::Circle(*body, 1.0, cpv(-40.0, 0.0));
-	shape->layers(Engine::Layers::PlayerHands);
-	shape->group(4);
 	shape->elasticity(0.0);
 	shape->friction(1.0);
 	body->addShape(shape);
@@ -36,35 +34,23 @@ Player::Player(PlayState& state)
 	_bodies["right_hand"] = new Chipmunk::Body(state.space(), 10.0, 50.0);
 	body = _bodies["right_hand"];
 	shape = new Chipmunk::Shapes::Circle(*body, 1.0, cpv(40.0, 0.0));
-	shape->group(4);
-	shape->layers(Engine::Layers::PlayerHands);
 	shape->elasticity(0.0);
 	shape->friction(1.0);
 	body->addShape(shape);
 
-	_bodies["head"] = new Chipmunk::Body(state.space(), 50.0, 50000000.0);
+	_bodies["head"] = new Chipmunk::Body(state.space(), 50.0, 5.0);
 	body = _bodies["head"];
 	shape = new Chipmunk::Shapes::Circle(*body, 5.0, cpv(0.0, 40.0));
 	shape->elasticity(0.0);
 	shape->friction(1.0);
 	body->addShape(shape);
 
-	/*
-	_bodies["bottom"] = new Chipmunk::Body(state.space(), 100.0, 1000.0);
-
-	Chipmunk::Shapes::Circle* circle = 0;
-		
-	circle = new Chipmunk::Shapes::Circle(_*body, 10.0, cpv(0.0, 20));
-	circle->elasticity(1.0);
-	circle->friction(1.0);
-	body->addShape(circle);
-	*/
-
 	state.space().addJoint(new Chipmunk::Joints::Slide(*_bodies["bottom"], *_bodies["center"], cpv(-1, 0), cpv(-1, 0), 0.0, 1.0));
 	state.space().addJoint(new Chipmunk::Joints::Slide(*_bodies["bottom"], *_bodies["center"], cpv( 1, 0), cpv( 1, 0), 0.0, 1.0));
-	state.space().addJoint(new Chipmunk::Joints::Slide(*_bodies["center"], *_bodies["left_hand"], cpv( 1, 0), cpv( 1, 0), 0.0, 2.0));
-	state.space().addJoint(new Chipmunk::Joints::Slide(*_bodies["center"], *_bodies["right_hand"], cpv( 1, 0), cpv( 1, 0), 0.0, 2.0));
-	state.space().addJoint(new Chipmunk::Joints::Slide(*_bodies["center"], *_bodies["head"], cpv( 1, 0), cpv( 1, 0), 0.0, 2.0));
+	//state.space().addJoint(new Chipmunk::Joints::Slide(*_bodies["center"], *_bodies["left_hand"], cpv( 1, 0), cpv( 1, 0), 0.0, 2.0));
+	//state.space().addJoint(new Chipmunk::Joints::Slide(*_bodies["center"], *_bodies["right_hand"], cpv( 1, 0), cpv( 1, 0), 0.0, 2.0));
+	state.space().addJoint(new Chipmunk::Joints::Slide(*_bodies["center"], *_bodies["head"], cpv(-1, 0), cpv(-1, 0), 0.0, 1.0));
+	state.space().addJoint(new Chipmunk::Joints::Slide(*_bodies["center"], *_bodies["head"], cpv( 1, 0), cpv( 1, 0), 0.0, 1.0));
 
 	typedef std::pair<std::string, Chipmunk::Body*> BodyPair;
 
@@ -88,29 +74,21 @@ Player::stop()
 void
 Player::goLeft()
 {
-	Chipmunk::Vector2 v = _bodies["center"]->velocity();
-	v.x -= 5.0;
-	v.y += 1.0;
-	_bodies["center"]->velocity(v);
+	_bodies["center"]->applyImpulse(cpv(-2000.0, 1.0), cpvzero);
 	_bottom->friction(0.0);
 }
 
 void
 Player::goRight()
 {
-	Chipmunk::Vector2 v = _bodies["center"]->velocity();
-	v.x += 5.0;
-	v.y += 1.0;
-	_bodies["center"]->velocity(v);
+	_bodies["center"]->applyImpulse(cpv(2000.0, 1.0), cpvzero);
 	_bottom->friction(0.0);
 }
 
 void Player::jump()
 {
 	Chipmunk::Vector2 v = _bodies["center"]->velocity();
-	v.y += 15.0;
-	//_bodies["center"]->velocity(v);
-	//_bodies["bottom"]->applyImpulse(cpv(0.0, -5000.0), cpvzero);
+	_bodies["bottom"]->applyImpulse(cpv(0.0, 5000.0), cpvzero);
 }
 
 void
