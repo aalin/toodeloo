b261470e5663cf5d850a958e2f4179ae8b951151
Andreas Alin <andreas.alin@gmail.com>
2009-03-31 19:52:54 +0200

    opt fix

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,10 @@ int SDL_main(int argc, char *argv[])
 int main(int argc, char *argv[])
 #endif
 {
-	std::string state;
 	po::options_description desc("Options");
 	desc.add_options()
 		("help", "show help")
-		("state", po::value<std::string>(&state)->default_value("gameplay"), "start with state. Valid states are gameplay and editor")
+		("editor", "start editor")
 	;
 
 	po::variables_map vm;
@@ -39,43 +38,41 @@ int main(int argc, char *argv[])
 	Toodeloo::Engine engine(1024, 768, false);
 
 	try {
-		if(state == "gameplay")
-		{
-			std::cout << "Starting gameplay" << std::endl;
-			engine.pushState(boost::shared_ptr<Toodeloo::States::State>(new Toodeloo::States::Gameplay(engine)));
-		}
-		else if(state == "editor")
+		if(vm.count("editor"))
 		{
 			std::cout << "Starting editor" << std::endl;
 			engine.pushState(boost::shared_ptr<Toodeloo::States::State>(new Toodeloo::States::Editor(engine)));
 		}
 		else
-			throw "unknown state!!!!!!!";
+		{
+			std::cout << "Starting gameplay" << std::endl;
+			engine.pushState(boost::shared_ptr<Toodeloo::States::State>(new Toodeloo::States::Gameplay(engine)));
+		}
+
+		int frames = 0;
+		int start_time = SDL_GetTicks();
+		while(engine.isRunning())
+		{
+			engine.handleInput();
+			engine.update();
+			engine.draw();
+
+			frames++;
+			int end_time = SDL_GetTicks();
+			if(end_time - start_time >= 5000)
+			{
+				float seconds = (end_time - start_time) / 1000.0;
+				float fps = frames / seconds;
+				std::cout << frames << " frames in " << seconds << " seconds: " << fps << " fps" << std::endl;
+				frames = 0;
+				start_time = end_time;
+			}
+		}
 	}
 	catch(const char* msg)
 	{
 		std::cout << msg << "\n";
 		return 1;
-	}
-
-	int frames = 0;
-	int start_time = SDL_GetTicks();
-	while(engine.isRunning())
-	{
-		engine.handleInput();
-		engine.update();
-		engine.draw();
-
-		frames++;
-		int end_time = SDL_GetTicks();
-		if(end_time - start_time >= 5000)
-		{
-			float seconds = (end_time - start_time) / 1000.0;
-			float fps = frames / seconds;
-			std::cout << frames << " frames in " << seconds << " seconds: " << fps << " fps" << std::endl;
-			frames = 0;
-			start_time = end_time;
-		}
 	}
 
 	SDL_Quit();
